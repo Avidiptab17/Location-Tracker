886e349387c96814c669160a16bfb71eec00cd5b
Avidipta Banerjee <107236731+Avidiptab17@users.noreply.github.com>
2023-07-13 20:14:19 +0530

    Update location.py

--- location.py
+++ location.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 def getdata(url):
 	r=requests.get(url)
 	return r.text
-api = 'b59a16f198e0a7f534f16ed78abfc87d'
-number = '9831003926'
-country = 'IN'
+api = 'Your API Key'
+number = 'Phone Number'
+country = 'Country Code'
 htmldata=getdata('http://apilayer.net/api/validate?access_key='+api+'&number='+number+'&country_code='+country+'&format=1')
 soup = BeautifulSoup(htmldata, 'html.parser')
 print(soup)
